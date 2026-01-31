a54a9792cff3d69edf264defa42251fe989ae459
Victor CHRISTMANN <98230507+Victor-CHRTM@users.noreply.github.com>
2026-01-31 21:25:22 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector(".main-nav");
+// Récupération des éléments HTML
+const burger = document.querySelector(".burger"); // Bouton burger
+const nav = document.querySelector(".main-nav"); // Menu de navigation
 
+// On met le bouton burger sur écoute afin de réagir lors d'un évènement "click"
 burger.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  document.body.style.overflow = nav.classList.contains("active")
+  nav.classList.toggle("active"); // On modifie la classe du menu lors d'un click = ajoute / enlève
+  document.body.style.overflow = nav.classList.contains("active") // On empêche le scroll une fois le menu ouvert
     ? "hidden"
     : "";
 });
